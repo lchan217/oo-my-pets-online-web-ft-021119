50a63990a12142572b52e1fce7f0355d2d3ace45
Lizzie Chan <lchan217@gmail.com>
2019-03-06 22:44:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -31,9 +31,10 @@ class Owner
     new_fish = Fish.new(name)
     @pets[:fishes] << new_fish
   end 
-  def buy_cat(name)
+  def 
+    buy_cat(name)
     new_cat = Cat.new(name)
-    @pets[:cats] << new_cat 
+    @pets[:cats] <<  new_cat  
   end 
   def buy_dog(name)
     new_dog = Dog.new(name)
@@ -47,7 +48,7 @@ class Owner
   end 
   def play_with_cats 
     @pets[:cats].each do |cat|
-      cat.mood = "happy"
+	    cat.mood = "happy"
     end
   end 
   def feed_fish
@@ -55,14 +56,14 @@ class Owner
       fish.mood = "happy"
     end
   end 
-  def sell_pets
-    pets.each do |pet, moods|
-      moods.each do |pet|
-        pet.mood = "nervous"
-      end 
-    end 
-    @pets.clear 
-  end 
+def sell_pets
+  @pets.each do |key, value|
+	  value.each do |mood|
+	    mood.mood = "nervous"
+	  end
+  end
+  @pets.clear
+end 
   def list_pets 
     return "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end 
